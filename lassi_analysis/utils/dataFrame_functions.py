d552dd7e98df377d367775d85c56bff4789617c3
okarahs <okara@hindsightsolutions.net>
2022-03-05 23:42:40 -0500

    return upper triangular

--- lassi_analysis/utils/dataFrame_functions.py
+++ lassi_analysis/utils/dataFrame_functions.py
@@ -18,7 +18,7 @@ def node2node_distances_dataframe(atomsdf):
                 for y in coordinates:
                     yield numpy.linalg.norm(x - y)
             return
-        return numpy.array(list(distance_generator(atomsdf))).reshape(len(atomsdf),len(atomsdf)) #reshape because we return a (len(atomsdf)xlen(atomsdf))x1 array otherwise. So we group len(n) elements at a time
+        return numpy.triu(numpy.array(list(distance_generator(atomsdf))).reshape(len(atomsdf),len(atomsdf))) #reshape because we return a (len(atomsdf)xlen(atomsdf))x1 array otherwise. So we group len(n) elements at a time
     
     def node_distances_dataframe(atomsdf:object):
         distances_matrix = node2node_distances(atomsdf)
